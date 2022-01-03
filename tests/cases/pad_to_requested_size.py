14efbf6b890c947d332cbff4d8dec65ecdc05f89
Eric Jelli <eric.jelli@caesar.de>
2022-01-03 18:22:51 +0100

    tests: Decouple test from "real" data

--- tests/cases/pad_to_requested_size.py
+++ tests/cases/pad_to_requested_size.py
@@ -37,10 +37,17 @@ def test_PadToRequestedSize(tmp_path):
     assert batch.arrays[raw].data.shape == (10, 10, 10)
 
 def test_PadToRequestedSize__real(tmp_path):
+
+    voxel_size = gp.Coordinate((1024, 1024, 35))
+
+    with zarr.open(tmp_path / 'sample.zarr', 'w') as f:
+        f['raw'] = np.zeros((1, 512, 100, 10))
+        f['raw'].attrs['resolution'] = tuple(voxel_size)
+
     raw = gp.ArrayKey('RAW')
 
     source = gp.ZarrSource(
-        str('/gpfs/soma_fs/home/jelli/src/Zebrafish_Retina_Semantic_Segmentation/training_data/train/macaque-retina-init-ds16/macaque-retina-init-ds16_bboxes_fp_top_narrow/1000-0-3671-2169-400-1.zarr'),
+        str(tmp_path / 'sample.zarr'),
         {raw: 'raw'},
         {raw: ArraySpec(
             interpolatable=True,
@@ -48,7 +55,6 @@ def test_PadToRequestedSize__real(tmp_path):
     )
 
     pipeline = source + RandomLocation() + PadToRequestedSize([raw])
-    voxel_size = gp.Coordinate((1024, 1024, 35))
     request = gp.BatchRequest()
     request[raw] = gp.ArraySpec(
         roi=gp.Roi((0, 0, 0), (256, 256, 1)) * voxel_size,
